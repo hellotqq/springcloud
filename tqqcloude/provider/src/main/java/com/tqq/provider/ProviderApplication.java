47f727e563b1b2c85af560f19c07bb6cfd1abfb0
tqq <2681082324@qq.com>
2020-09-27 09:47:47 +0800

    为服务注册中心  服务注册与消费

---
+++ tqqcloude/provider/src/main/java/com/tqq/provider/ProviderApplication.java
@@ -0,0 +1,13 @@
+package com.tqq.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+
+}
--- tqqcloude/provider/src/main/java/com/tqq/provider/RegisterController.java
+++ tqqcloude/provider/src/main/java/com/tqq/provider/RegisterController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.ws.rs.POST;
-
 /**
  * @author ： tqq
  * @date ： 2020/9/26 22:31
